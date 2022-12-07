cb9f37a612d0988112f5d28726f0adaf3e4190b9
satoshi <mimizunobouken@gmail.com>
2022-12-07 18:05:45 +0900

    Servicesコンポーネントにテキストスタイルを追加

--- components/Services.js
+++ components/Services.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Title from "./Title";
+import { ShoppingCart } from "react-feather";
+import Link from "next/link";
+import styles from "../styles/Services.module.css";
 const Services = () => {
   return (
     <section className="parts-grid section-padding">
       <Title title={"Services"} />
+      <div className={styles.boxs}>
+        <div className={styles.box}>
+          <h3>販売</h3>
+          <ShoppingCart />
+          <p className={styles.desc}>
+            各種製造・分析装置の販売を行なっています。お気軽にご相談ください。
+          </p>
+          <Link href={"/"} className={styles.link}>
+            取り扱い商品一覧はコチラ
+          </Link>
+        </div>
+      </div>
     </section>
   );
 };
