deae0ac70ff8258fe5585266c5b9743dc0f752ff
satoshi <mimizunobouken@gmail.com>
2022-12-07 13:40:15 +0900

    setup

---
+++ libs/SiteMeta.js
@@ -0,0 +1,10 @@
+const Meta = {
+  siteTitle: "Trust Technology Corporation",
+  siteDesc: "Trust Technology Corporationのリデザインサイトです",
+  siteUrl: ``,
+  siteLang: "ja",
+  siteLocale: "ja_JP",
+  siteType: "website",
+  siteIcon: "/favicon.png",
+};
+export default Meta
