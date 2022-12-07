f89dc50b7c9434c9523c9e77f33ca2128bc69104
satoshi <mimizunobouken@gmail.com>
2022-12-07 21:04:51 +0900

    Footerコンポーネントにテキストスタイルを追加

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,62 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../styles/Footer.module.css";
+import Link from "next/link";
+import Meta from "../libs/SiteMeta";
+import Image from "next/image";
+import { footerLinks } from "../libs/links";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className={`parts-grid section-padding ${styles.bcg}`}>
+      <nav>
+        {footerLinks.map((item, i) => {
+          const { name, subItem, box } = item;
+          return (
+            <div className={`${styles.box} ${styles[box]}`} key={i}>
+              <h4 className={styles.navtitle}>{name}</h4>
+              <ul>
+                {subItem.map((item, i) => {
+                  const { name, path } = item;
+                  return (
+                    <li key={i}>
+                      <Link href={path} className={styles.navname}>{name}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </nav>
+      <div className={styles.siteInfo}>
+        <div className={styles.titleBox}>
+          <Link href="/">
+            <Image
+              src={Meta.siteIcon}
+              width={22}
+              height={22}
+              className={styles.favicon}
+              alt={"favicon"}
+            />
+            <p className={styles.siteTitle}>{Meta.siteTitle}</p>
+          </Link>
+        </div>
+        <p className={styles.address}>
+          〒160-0023 東京都新宿区西新宿 6-12-1 パークウエスト13F
+        </p>
+        <small className={styles.copyright}>
+          ©Copyright 2005-2022 (TTC Group) Trust Technology Co, LTD All rights
+          reserved.
+        </small>
+      </div>
+      <Image
+        src="/footer.png"
+        width={362}
+        height={243}
+        className={styles.heroImg}
+        alt="footer-image"
+      />
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
--- libs/links.js
+++ libs/links.js
@@ -18,4 +18,25 @@ const contact = [
   },
 ];
 
-export { links, contact };
+const footerLinks = [
+  {
+    name: "会社情報",
+    box: "box1",
+    subItem: [
+      { name: "本社", path: "/" },
+      { name: "採用", path: "/" },
+      { name: "アクセス", path: "/" },
+    ],
+  },
+  {
+    name: "製品情報",
+    box: "box2",
+    subItem: [
+      { name: "販売製品一覧", path: "/" },
+      { name: "レンタル製品一覧", path: "/" },
+      { name: "買取製品一覧", path: "/" },
+    ],
+  },
+];
+
+export { links, contact, footerLinks };
