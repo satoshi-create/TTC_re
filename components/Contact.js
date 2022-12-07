cdea1622619ce56f7e8952f49e5316c27e648968
satoshi <mimizunobouken@gmail.com>
2022-12-07 20:26:19 +0900

    Companyコンポーネントにテキストスタイルを追加

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Title from "./Title";
+import Image from "next/image";
+import styles from "../styles/Contact.module.css";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Select,
+  Textarea,
+  Input,
+} from "@chakra-ui/react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="parts-grid section-padding">
+      <Title title={"Company"} />
+      <FormControl isRequired>
+        <Input placeholder="名前" className={styles.input} />
+      </FormControl>
+      <FormControl isRequired>
+        <Input placeholder="メールアドレス" className={styles.input} />
+      </FormControl>
+      <FormControl>
+        <Select placeholder="お問い合わせ内容" className={styles.input}>
+          <option>採用について</option>
+          <option>ビジネスについて</option>
+        </Select>
+      </FormControl>
+      <Textarea
+        placeholder="メッセージ"
+        size="sm"
+        className={`${styles.input} ${styles.textarea}`}
+      />
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
