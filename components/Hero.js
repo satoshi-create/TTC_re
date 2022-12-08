eaf223c88106bf3a53a3362691534b345d02cce4
satoshi <mimizunobouken@gmail.com>
2022-12-08 11:00:35 +0900

    Heroコンポーネントにレイアウトスタイルを追加

--- components/Hero.js
+++ components/Hero.js
@@ -6,20 +6,26 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <section className={`parts-grid ${styles.bcg}`}>
+      <div className={styles.box}>
         <h1 className={styles.catch}>
-          半導体と液晶パネルを<br/>製造する装置の<br/>プロフェッショナル
+          半導体と液晶パネルを
+          <br />
+          製造する装置の
+          <br />
+          プロフェッショナル
         </h1>
 
         <Button value={{ path: "/", name: "装置一覧を見る" }} />
-        {/* <figure>
-          <Image
-            src="/hero.png"
-            width={250}
-            height={200}
-            className={styles.heroImg}
-            alt="hero-image"
-          />
-        </figure> */}
+      </div>
+      <figure className={styles.figure}>
+        <Image
+          src="/hero.png"
+          width={250}
+          height={200}
+          className={styles.heroImg}
+          alt="hero-image"
+        />
+      </figure>
     </section>
   );
 };
